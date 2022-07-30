c474331360b4354909ed8ee22f739d29950906f1
Samsud Dhuha <samsud@paxel.co>
2022-07-30 10:18:54 +0700

    change pool function

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -2,7 +2,7 @@ const db = require('../config/dbConfig.js');
 const bcrypt = require("bcryptjs");
 
 exports.getUsers = (request, response) => {
-    db.query('SELECT * FROM user_apps ORDER BY id ASC', (error, results) => {
+    db.pool.query('SELECT * FROM user_apps ORDER BY id ASC', (error, results) => {
         if (error) {
             throw error
         }
