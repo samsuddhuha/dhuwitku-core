ae442dcffd3d7e7d850aa12c2f236110ee6e8942
Samsud Dhuha <samsud.dhuha@mamiteam.com>
2023-12-24 00:35:14 +0700

    update version controller

--- app/controllers/versionController.js
+++ app/controllers/versionController.js
@@ -5,13 +5,13 @@ const baseError = require("../middleware/error.js");
 exports.getVersion = (request, response) => {
     const reqVersionSplit = request.body.version.split(".");
 
-    db.pool.query("SELECT * FROM m_version", (error, results) => {
+    db.pool.query("SELECT * FROM m_version ORDER BY created_at DESC", (error, results) => {
         baseError.handleError(error, response)
 
         var updateApps = false
         var message = "Aplikasi sudah version terbaru"
 
-        let version = results[results.length - 1].android_version;
+        let version = results[0].android_version;
         let versionSplit = version.split(".");
         for (const index in versionSplit) { 
             let reqVersion = parseInt(reqVersionSplit[index])
@@ -25,7 +25,7 @@ exports.getVersion = (request, response) => {
         let  data = {
             new_version_apps: version,
             update_apps: updateApps,
-            force_update: results[results.length - 1].force_update
+            force_update: results[0].force_update
         };
         let status = updateApps ? statusCode.update_aplication : statusCode.success
         response.status(status).json({
