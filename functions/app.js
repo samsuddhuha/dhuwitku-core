eded8ddf3fc2671b5d08cc6c185f2a2a3d6add47
Samsud Dhuha <samsud.dhuha@mamiteam.com>
2024-06-08 00:00:14 +0700

    init netlify

--- functions/app.js
+++ functions/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const serverless = require("serverless-http");
+const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
@@ -7,21 +7,21 @@ var bodyParser = require('body-parser');
 
 let routerApps = require('../app/routers/router.js');
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.get('/', (req, res) => {
 	res.send({
     message: "Welcome to DhuwitKu"
   });
 });
-app.use('/', routerApps);
+// app.use('/', routerApps);
 // app.use('/resource', express.static('./resource'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname);
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+// app.set('views', __dirname);
 
-app.get('/privacypolicy', function(req, res){
-  res.render('./app/view/privacypolicy');
-});
+// app.get('/privacypolicy', function(req, res){
+//   res.render('./app/view/privacypolicy');
+// });
 
 app.use("/.netlify/functions/app", router);
 module.exports.handler = serverless(app);
