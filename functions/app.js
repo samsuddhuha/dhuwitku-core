b74369d1ff366f53e613ba4fcfd82bc482e5c2fd
Samsud Dhuha <samsud.dhuha@mamiteam.com>
2024-06-08 01:50:47 +0700

    change netlify

--- functions/app.js
+++ functions/app.js
@@ -1,27 +1,24 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
-const router = express.Router();
+const path = require("path");
 
-var bodyParser = require('body-parser');
-
-let routerApps = require('../app/routers/router.js');
-
-// app.use(bodyParser.json());
-app.get('/', (req, res) => {
-	res.send({
-    message: "Welcome to DhuwitKu"
-  });
-});
-// app.use('/', routerApps);
-// app.use('/resource', express.static('./resource'));
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+// router.get('/', (req, res) => {
+// 	res.send("Welcome to DhuwitKu");
+// });
+// router.use('/', routerApps);
+// router.use('/resource', express.static('./resource'));
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
-// app.set('views', __dirname);
+// const publicPath = path.join(__dirname, '../app');
+// app.set('views', publicPath);
 
-// app.get('/privacypolicy', function(req, res){
-//   res.render('./app/view/privacypolicy');
+let routerApps = require('../app/routers/router.js');
+// routerApps.get('/privacypolicy', function(req, res){
+//   res.render('../app/view/privacypolicy');
 // });
 
-app.use("/.netlify/functions/app", router);
+app.use("/.netlify/functions/app", routerApps);
 module.exports.handler = serverless(app);
